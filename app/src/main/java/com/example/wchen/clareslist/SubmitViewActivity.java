79c4de1b2add96b41f999b043e53e88d36e0b5ae
williumchen <wichen@g.hmc.edu>
2015-10-06 12:45:40 -0700

    submit button

--- app/src/main/java/com/example/wchen/clareslist/SubmitViewActivity.java
+++ app/src/main/java/com/example/wchen/clareslist/SubmitViewActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by wchen on 10/5/15.
@@ -13,8 +16,17 @@ public class SubmitViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_view);
-
         Intent intent = getIntent();
+
+        final Button submitBtn = (Button) findViewById(R.id.submit_button);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+        EditText newItem = (EditText) findViewById(R.id.submit_item);
+        EditText newDesc = (EditText) findViewById(R.id.submit_description);
     }
 
     @Override
