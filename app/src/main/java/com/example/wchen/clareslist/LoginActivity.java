b293325a9f224b8ff6bccb2d2b2c192ef37f12e2
efirst <efirst@hmc.edu>
2015-12-11 19:11:54 -0800

    fixed login

--- app/src/main/java/com/example/wchen/clareslist/LoginActivity.java
+++ app/src/main/java/com/example/wchen/clareslist/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.example.wchen.clareslist;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -25,11 +28,21 @@ public class LoginActivity extends Activity {
         // takes us to the next screen
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                pw.maybeLogInUser(email.getText().toString(), "password");
-                pw.setCurrentUser();
-                Intent nextScreen = new Intent(v.getContext(), IntermediateActivity.class);
-                v.getContext().startActivity(nextScreen);
-                finish();
+                if (TextUtils.isEmpty(email.getText().toString().trim()))
+                {
+                    Context context = getApplicationContext();
+                    CharSequence text = "You forgot to enter your email!";
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+                else {
+                    pw.maybeLogInUser(email.getText().toString(), "password");
+                    pw.setCurrentUser();
+                    Intent nextScreen = new Intent(v.getContext(), IntermediateActivity.class);
+                    v.getContext().startActivity(nextScreen);
+                    finish();
+                }
             }
         });
     }
